cddcff46ab0f31abbef12c153c4fd9850c8f9e64
Andrea Salcedo <andreacsalcedo@gmail.com>
2013-11-11 21:56:41 -0430

    Scrabble.py: Corregidos algunos comentarios y ahora las palabras de un archivo se almacenan en una lista.

--- Codigo/Scrabble.py
+++ Codigo/Scrabble.py
@@ -3,7 +3,7 @@
 
 # ------------------------------------------------------------------ #
 # Scrabble.py                                                        #
-# Programa que determina cuales letras nunca aperecen repetidas de   #
+# Programa que determina cuales letras no aperecen repetidas de      #
 # forma consecutiva en las palabras leidas de un archivo de texto.   #
 # Autor: Andrea Salcedo                                              #
 # Fecha de ultima modificacion: 11/11/13                             #
@@ -12,9 +12,9 @@
 import sys
 import string
 
-""" Funcion que lee las palabras de un archivo y verifica si alguna
-letra aperece repitida de forma consecutiva. """
-def buscarDuplicadas(archivo):
+""" Funcion que lee las palabras de una lista y verifica si alguna
+letra aperece repetida de forma consecutiva. """
+def buscarDuplicadas(palabras):
    
       listaLetras = map(chr, range(65,91))
       noDuplicadas = []
@@ -24,16 +24,16 @@ def buscarDuplicadas(archivo):
          dobleLetra = letra+letra;
          esDuplicada = False;
          
-         for palabra in archivo:
-         
-         """ Si la palabra contiene la letra duplicada, entonces se sale 
-         del ciclo y busca la proxima letra en la palabra actual. """
-            if (palabra.find(dobleLetra) >= 0):
+         for palabra in palabras:
+                   
+         # Si la palabra actual contiene la letra duplicada, entonces se 
+         # sale del ciclo y sigue a la proxima letra.
+            if (dobleLetra in palabra):
                esDuplicada = True
                break
             
-         """ Si despues de buscar en todas las palabras la letra no fue
-         repetida, entonces se agrega a la lista de letras no duplicadas. """
+         # Si la letra actual no fue repetida despues de buscar en todas las 
+         # palabras, entonces se agrega a la lista de letras no duplicadas.         
          if not (esDuplicada):
             noDuplicadas.append(letra)
                
@@ -43,13 +43,14 @@ def main():
    
    try:
       archivo = open(sys.argv[1])
-      noDuplicadas = buscarDuplicadas(archivo)
+      palabras = archivo.readlines()
+      noDuplicadas = buscarDuplicadas(palabras)
       print noDuplicadas 
       archivo.close()
                
    except IOError, ex:
       print "IOError", ex
    except IndexError:
-      print "No hay suficiente argumentos: ./Scrabble archivo.txt "
+      print "No hay suficiente argumentos: ./Scrabble.py archivo.txt "
       
 main()
